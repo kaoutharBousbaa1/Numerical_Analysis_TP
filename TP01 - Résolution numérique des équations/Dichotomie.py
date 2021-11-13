daa413c82b403bb37cd20ad0e0d4e85c218c4f12
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-14 00:22:57 +0100

    Update Dichotomie.py

--- TP01 - Résolution numérique des équations/Dichotomie.py
+++ TP01 - Résolution numérique des équations/Dichotomie.py
@@ -1,17 +1,17 @@
 import math
-def f(x)
+def f(x):
     x = x**2 -2
 def dichotomie(f,a,b,e):
     i = 0
-    while(i < e)
+    while(i < e):
         alpha = (a + b) / 2
-        if(f(alpha) >= 0)
+        if(f(alpha) >= 0):
             b = alpha
-        else
+        else:
             a = alpha
         i = i + 1
     return alpha
-def dichotomie2(f, a, b, e)
+def dichotomie2(f, a, b, e):
     n = int(math.log((b - a)[2]) /(e * math.log((2)[2]))) + 1
     alpha = dichotomie(f, a, b, n)
     return alpha
