d7d8cfc622dccfcf39ce27b129fc66e853290f03
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-14 00:52:05 +0100

    Update EX05.py

--- TP01 - Résolution numérique des équations/EX05.py
+++ TP01 - Résolution numérique des équations/EX05.py
@@ -1,11 +1,11 @@
 from matplotlib.pylab import plot, show
-def f(x)
+def f(x):
     x = x**3 - x - 3
-def g(y)
+def g(y):
     y = 3**2 - 1
-def Newton(f, g, alpha, n)
+def Newton(f, g, alpha, n):
     l = [0]
-    while(n <= 100)
+    while(n <= 100):
         alpha = alpha - f(alpha) / g(alpha)
         n = n + 1
 	l = l + [alpha]
@@ -13,10 +13,7 @@ def Newton(f, g, alpha, n)
     show()
     grid()
     return alpha
-
 n = 0
-print("------------------------Pour alpha_zero = 0----------------------------\n")
-alpha = Newton(f, g, 0, n)
-print("------------------------Pour alpha_zero = 1----------------------------\n")
-alpha = Newton(f, g, 1, n)
+print("------------------------Pour alpha_zero = 0----------------------------\n", Newton(f, g, 0, n))
+print("------------------------Pour alpha_zero = 1----------------------------\n", Newton(f, g, 1, n))
 
